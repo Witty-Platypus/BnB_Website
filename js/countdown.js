1c2442d8e300a3dd12da8426a018132a0cbf2d5b
ineesalmeida <ineesalmeida@gmail.com>
2022-11-16 21:30:16 +0000

    Place release countdown above buttons

--- js/countdown.js
+++ js/countdown.js
@@ -4,29 +4,26 @@ var countDownDate = new Date("Nov 22, 2022 18:30:00 GMT").getTime();
 // Update the count down every 1 second
 var x = setInterval(function() {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+    // Get today's date and time
+    var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("countdown_timer").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+    // Display the result in the element with id="demo"
+    document.getElementById("pre-countdown").innerHTML = "Releasing in...";
+    document.getElementById("countdown_timer").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown_timer").innerHTML = "OUT NOW!";
-    var elements = document.getElementsByClassName("countdown_timer_wrapper");
-    while(elements.length > 0){
-        elements[0].removeChildren();
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").remove();
     }
-  }
 }, 1000);
